4a7abf3921586d8720d2c7f4eae909a70b1f4b33
HaasResearch <haasresearchgroup@gmail.com>
2016-12-09 13:22:09 -0600

    Update and rename setup.sh to update.sh

--- update.sh
+++ update.sh
@@ -15,13 +15,12 @@ echo You Chose: $varname
 
 if [ $varname == '1' ]
 then
-git clone https://github.com/HaasResearch/devops
-#  echo "1 - You Chose >>> Install or Update HRG"
-echo "hello there!"
-echo -e "export PATH=$PATH:~/bin \n" >> $HOME/.bashrc
-mkdir ~/bin
-source $HOME/.bashrc
-
+  git clone https://github.com/HaasResearch/devops
+  #  echo "1 - You Chose >>> Install or Update HRG"
+  echo "hello there!"
+  echo -e "export PATH=$PATH:~/bin \n" >> $HOME/.bashrc
+  mkdir ~/bin
+  source $HOME/.bashrc
 fi
 
 
