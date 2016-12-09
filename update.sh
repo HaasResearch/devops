4c8649191e7136ba5a3c8064fad7486ec218c97b
HaasResearch <haasresearchgroup@gmail.com>
2016-12-09 13:27:18 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -15,9 +15,8 @@ echo You Chose: $varname
 
 if [ $varname == '1' ]
 then
+  echo "1 - You Chose >>> Install or Update HRG"
   git clone https://github.com/HaasResearch/devops
-  #  echo "1 - You Chose >>> Install or Update HRG"
-  echo "hello there!"
   echo -e "export PATH=$PATH:~/bin \n" >> $HOME/.bashrc
   mkdir ~/bin
   source $HOME/.bashrc
